bd1c9f3d072a7ac78f4dd3df763ded48fb1368e7
Michael Poore <michael@michaelpoore.com>
2022-10-28 15:55:06 +0100

    1.0.1

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+VERSION=$(<VERSION)
+docker build . -t v12nio/codestream-ci:latest -t v12nio/codestream-ci:v$VERSION --build-arg VERSION=$VERSION
+docker login --username $DOCKERUSER --password $DOCKERPASS
+docker push -a v12nio/codestream-ci
